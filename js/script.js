33a2c12cb044e3261f259688ee128cebbb445d8d
Ni-Han-402 <59785191+Ni-Han-402@users.noreply.github.com>
2020-04-06 23:44:11 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  autoplay: true,
+  autoplayTimeout: 7000,
+  autoplayHoverPause: true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:2
+      }
+  }
+})
+
+
+$(document).ready(function(){
+    AOS.init();
+})
+
